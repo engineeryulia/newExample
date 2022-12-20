aa43546771dafdfe9ad791eb1bff724586db9826
Yulia Lopatko <engineeryulia@gmail.com>
2022-12-20 16:29:59 -0700

    sec

--- cypress/e2e/firstTest.cy.js
+++ cypress/e2e/firstTest.cy.js
@@ -4,8 +4,15 @@ describe('My First Test', () => {
   beforeEach(() => {
   cy.visit('https://example.cypress.io')
   });
+
   it('clicking "type" navigate to a new url', () => {
     cy.contains('next').click()
-    cy.url().should('include', 'command/traversal')
+    cy.url().should('include', 'commands/traversal')
+  })
+
+  it('clicking "root" navigates to a new url', () => {
+    cy.contains('root').click()
+
+
   })
 })
